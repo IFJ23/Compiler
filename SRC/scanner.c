8e0f3c1005f34c918db482a254d5cf24b7d51324
Im0ne <116801019+Im0ne@users.noreply.github.com>
2023-10-10 15:50:32 +0200

    beginning of beginning

---
+++ SRC/scanner.c
@@ -0,0 +1,6 @@
+// Compiler to IFJ23 language
+// Faculty of Information Technology Brno University of Technology
+// Authors:
+// Ivan Onufriienko (xonufr00)
+
+#include "scanner.h"
---
+++ SRC/scanner.h
@@ -0,0 +1,15 @@
+// Compiler to IFJ23 language
+// Faculty of Information Technology Brno University of Technology
+// Authors:
+// Ivan Onufriienko (xonufr00)
+
+#ifndef IFJ23_SCANNER_H 
+#define IFJ23_SCANNER_H
+
+#include "error.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+#endif //IFJ23_SCANNER_H
