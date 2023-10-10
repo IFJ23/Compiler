8a293aa66b8436cfbb232367e446fb90cbd4164c
Im0ne <116801019+Im0ne@users.noreply.github.com>
2023-10-10 16:11:12 +0200

    creating some structures

--- SRC/scanner.h
+++ SRC/scanner.h
@@ -10,6 +10,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef enum {
+    KW_DOUBLE,
+    KW_ELSE,
+    KW_FUNC,
+    KW_IF,
+    KW_INT,
+    KW_LET,
+    KW_NIL,
+    KW_RETURN,
+    KW_STRING,
+    KW_VAR,
+    KW_WHILE,
+} Keyword;
+
+typedef enum {
+    TYPE_KW,
+    TYPE_ID,
+    TYPE_EOL,
+    TYPE_EOF,
+    TYPE_INT,
+    TYPE_DOUBLE,
+    TYPE_STRING,
+    TYPE_PLUS,
+    TYPE_MINUS,
+    TYPE_MUL,
+    TYPE_DIV,
+    TYPE_LESS,
+    TYPE_MORE,
+    TYPE_LESS_EQUAL,
+    TYPE_MORE_EQUAL,
+    TYPE_EQUAL,
+    TYPE_NOT_EQUAL,
+    TYPE_ASSIGN,
+    TYPE_LEFT_BRACKET,
+    TYPE_RIGHT_BRACKET,
+    TYPE_COMMA,
+    TYPE_COLON,
+} Token_type;
 
 
 #endif //IFJ23_SCANNER_H
