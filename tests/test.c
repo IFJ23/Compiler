83b692654e6388c709cc3b11ccb8b2399bc2897a
Dews <macbook@MacBook-MacBook-Air-2.local>
2023-10-20 22:40:59 +0200

    extended test 1

--- tests/test.c
+++ tests/test.c
@@ -9,7 +9,7 @@
 #include "../SRC/scanner.h" // Include the header file of your code
 
 // Define a helper function to run tests
-void run_test(const char *test_name, int expected_type, int expected_line, FILE *input_file) {
+void run_test(const char *test_name, Token_type expected_type, int expected_line, FILE *input_file) {
     Scanner scanner;
     scanner.file = input_file;
     Token token;
@@ -24,20 +24,40 @@ void run_test(const char *test_name, int expected_type, int expected_line, FILE 
 
 int main() {
     // Create input files for your test cases
-    FILE *test_input1 = fopen("test1.txt", "r");
-    FILE *test_input2 = fopen("test2.txt", "r");
+    FILE *test_input = fopen("test_cases.txt", "r");
 
     // Test 1: Check for TYPE_PLUS
-    run_test("Test for TYPE_PLUS", TYPE_PLUS, 1, test_input1);
+    // Test all the cases from test_cases.txt
+    run_test("Test for TYPE_PLUS", TYPE_PLUS, 0, test_input);
+    run_test("Test for TYPE_MINUS", TYPE_MINUS, 0, test_input);
+    run_test("Test for TYPE_MUL", TYPE_MUL, 0, test_input);
+    run_test("Test for TYPE_DIV", TYPE_DIV, 0, test_input);
+    run_test("Test for TYPE_LESS", TYPE_LESS, 0, test_input);
+    run_test("Test for TYPE_LESS_EQUAL", TYPE_LESS_EQUAL, 0, test_input);
+    run_test("Test for TYPE_MORE", TYPE_MORE, 0, test_input);
+    run_test("Test for TYPE_MORE_EQUAL", TYPE_MORE_EQUAL, 0, test_input);
+    run_test("Test for TYPE_ASSIGN", TYPE_ASSIGN, 0, test_input);
+    run_test("Test for TYPE_EQUAL", TYPE_EQUAL, 0, test_input);
+    run_test("Test for TYPE_NOT_EQUAL", TYPE_NOT_EQUAL, 0, test_input);
+    run_test("Test for TYPE_LEFT_BRACKET", TYPE_LEFT_BRACKET, 0, test_input);
+    run_test("Test for TYPE_RIGHT_BRACKET", TYPE_RIGHT_BRACKET, 0, test_input);
+    run_test("Test for TYPE_COMMA", TYPE_COMMA, 0, test_input);
+    run_test("Test for TYPE_COLON", TYPE_COLON, 0, test_input);
+    run_test("Test for TYPE_LEFT_CURLY_BRACKET", TYPE_LEFT_CURLY_BRACKET, 0, test_input);
+    run_test("Test for TYPE_RIGHT_CURLY_BRACKET", TYPE_RIGHT_CURLY_BRACKET, 0, test_input);
+    run_test("Test for TYPE_NIL_COALESCING_OPERATOR", TYPE_NIL_COALESCING_OPERATOR, 0, test_input);
+    run_test("Test for TYPE_ID", TYPE_ID, 0, test_input);
+    run_test("Test for TYPE_KW (Keyword)", TYPE_KW, 0, test_input);
+    run_test("Test for TYPE_KW (Keyword)", TYPE_KW, 0, test_input);
+    run_test("Test for TYPE_STRING", TYPE_STRING, 0, test_input);
+    run_test("Test for TYPE_INT", TYPE_INT, 0, test_input);
+    run_test("Test for TYPE_DOUBLE", TYPE_DOUBLE, 0, test_input);
 
-    // Test 2: Check for TYPE_RETURN_ARROW
-    run_test("Test for TYPE_RETURN_ARROW", TYPE_RETURN_ARROW, 2, test_input2);
 
     // Add more test cases here for other token types, keywords, and identifiers
 
     // Close input files
-    fclose(test_input1);
-    fclose(test_input2);
+    fclose(test_input);
 
     printf("All tests passed!\n");
     return 0;
