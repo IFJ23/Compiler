d7d1e1e49005cf9705ba42f4d6691c7c199622ac
Vsevolod Pokhvalenko <vsevolod.pohvalenko@gmail.com>
2023-12-06 15:34:05 +0100

    comments symstruc

--- SRC/symstack.c
+++ SRC/symstack.c
@@ -6,53 +6,59 @@
 
 #include "symstack.h"
 
+// Function to initialize the stack
 void stackInit(Stack *s)
 {
-    s->head = NULL;
-    s->len = 0;
+    s->head = NULL;  // Set the head of the stack to NULL, indicating an empty stack
+    s->len = 0;      // Initialize the length of the stack to 0
 }
 
+// Function to push a token onto the stack
 void stackPush(Stack *s, Token t)
 {
-    if (s == NULL)
+    if (s == NULL)    // Check if the stack is NULL
         return;
 
+    // Allocate memory for a new stack item
     StackItem *newItem = malloc(sizeof(StackItem));
-    if (newItem == NULL)
+    if (newItem == NULL)  // Check if memory allocation failed
         return;
 
-    newItem->t = t;
-    newItem->next = s->head;
-    s->head = newItem;
-    ++(s->len);
+    newItem->t = t;             // Set the token in the new stack item
+    newItem->next = s->head;    // Set the next pointer of the new item to the current top of the stack
+    s->head = newItem;          // Update the head of the stack to the new item
+    ++(s->len);                 // Increment the length of the stack
 }
 
+// Function to pop a token from the stack
 void stackPop(Stack *s, Token *t)
 {
-    if (s == NULL || s->head == NULL)
+    if (s == NULL || s->head == NULL)  // Check if the stack or the head of the stack is NULL
         return;
 
+    // Temporary variable to hold the current top of the stack
     StackItem *tmp = s->head;
     if (t != NULL)
-        *t = tmp->t;
-    s->head = s->head->next;
-    free(tmp);
-    --(s->len);
+        *t = tmp->t;            // If the 't' pointer is not NULL, store the token in the provided location
+    s->head = s->head->next;    // Update the head of the stack to the next item
+    free(tmp);                  // Free the memory of the popped item
+    --(s->len);                 // Decrement the length of the stack
 }
 
+// Function to free the memory used by the stack
 void stackFree(Stack *s)
 {
-    if (s == NULL)
+    if (s == NULL)   // Check if the stack is NULL
         return;
 
+    // Temporary variable to traverse the stack
     StackItem *tmp = s->head;
     while (tmp != NULL)
     {
-        tmp = s->head->next;
-        free(s->head);
-        s->head = tmp;
+        tmp = s->head->next;   // Move to the next item in the stack
+        free(s->head);         // Free the memory of the current item
+        s->head = tmp;         // Update the head of the stack
     }
 
-    s->len = 0;
+    s->len = 0;  // Set the length of the stack to 0
 }
-
--- SRC/symtable.c
+++ SRC/symtable.c
@@ -8,105 +8,109 @@
 #include <stdint.h>
 #include "symtable.h"
 
+// Function to calculate the hash value for a given key
 size_t symtableHash(const char *str)
 {
-    uint32_t h = 0;
-    const unsigned char *p;
+    uint32_t h = 0;                         // Initialize the hash value to 0
+    const unsigned char *p;                // Pointer to traverse the string
     for (p = (const unsigned char *)str; *p != '\0'; p++)
-        h = 65599 * h + *p;
-    return h;
+        h = 65599 * h + *p;                // Hashing algorithm (a prime number used as a multiplier)
+    return h;                              // Return the calculated hash value
 }
 
+// Function to initialize a symbol table with a given size
 Symtable *symtableInit(size_t n)
 {
-    Symtable *table = malloc(sizeof(Symtable));
+    Symtable *table = malloc(sizeof(Symtable));  // Allocate memory for the symbol table
     if (table == NULL)
         return NULL;
 
-    table->size = 0;
-    table->arr_size = n;
-    table->arr_ptr = malloc(sizeof(SymtableItem *) * n);
+    table->size = 0;                                            // Initialize the number of elements to 0
+    table->arr_size = n;                                        // Set the size of the array of linked lists
+    table->arr_ptr = malloc(sizeof(SymtableItem *) * n);   // Allocate memory for the array of linked lists
     if (table->arr_ptr == NULL)
     {
-        free(table);
+        free(table);                // Free the previously allocated memory if the allocation fails
         return NULL;
     }
 
     for (size_t i = 0; i < n; i++)
     {
-        table->arr_ptr[i] = NULL;
+        table->arr_ptr[i] = NULL;   // Initialize each linked list in the array to NULL
     }
 
-    return table;
+    return table;                   // Return the initialized symbol table
 }
 
+// Function to find a key in the symbol table and return its associated data
 SymtablePair *symtableFind(Symtable *t, SymtableKey key)
 {
     if (t == NULL)
         return NULL;
 
-    size_t hash = symtableHash(key) % t->arr_size;
-    SymtableItem *tmp = t->arr_ptr[hash];
+    size_t hash = symtableHash(key) % t->arr_size;   // Calculate the hash value for the key
+    SymtableItem *tmp = t->arr_ptr[hash];            // Get the linked list at the calculated index
 
     while (tmp != NULL)
     {
         if (!strcmp(key, tmp->pair.key))
         {
-            return &(tmp->pair);
+            return &(tmp->pair);    // Return the pair if the key is found
         }
-        tmp = tmp->next;
+        tmp = tmp->next;            // Move to the next item in the linked list
     }
-    return NULL;
+    return NULL;                    // Return NULL if the key is not found
 }
 
+// Function to add a key-value pair to the symbol table
 SymtablePair *symtableAdd(Symtable *t, SymtableKey key, ElemType type, int paramsCnt, bool undefined, LinkedList params)
 {
     if (t == NULL)
         return NULL;
 
-    SymtablePair *found = symtableFind(t, key);
+    SymtablePair *found = symtableFind(t, key);       // Check if the key already exists in the symbol table
 
     if (found != NULL)
-        return found;
+        return found;                                // Return the existing pair if the key is found
 
-    SymtableItem *newitem = malloc(sizeof(SymtableItem));
+    SymtableItem *newitem = malloc(sizeof(SymtableItem));  // Allocate memory for a new item in the linked list
     if (newitem == NULL)
     {
-        return NULL;
+        return NULL;                                    // Return NULL if memory allocation fails
     }
 
-    size_t len = strlen(key);
-    newitem->pair.key = calloc(len + 1, 1);
+    size_t len = strlen(key);                        // Calculate the length of the key
+    newitem->pair.key = calloc(len + 1, 1);  // Allocate memory for the key and initialize to 0
     if (newitem->pair.key == NULL)
     {
-        free(newitem);
+        free(newitem);                                  // Free the allocated memory if allocation fails
         return NULL;
     }
 
-    memcpy((char *)newitem->pair.key, key, len);
-    newitem->pair.value.type = type;
-    newitem->pair.value.paramsCnt = paramsCnt;
-    newitem->pair.value.possiblyUndefined = undefined;
-    newitem->pair.value.parameters = params;
-    newitem->next = NULL;
-    t->size++;
+    memcpy((char *)newitem->pair.key, key, len);        // Copy the key to the allocated memory
+    newitem->pair.value.type = type;                     // Set the type in the value structure
+    newitem->pair.value.paramsCnt = paramsCnt;           // Set the number of parameters in the value structure
+    newitem->pair.value.possiblyUndefined = undefined;   // Set the possiblyUndefined flag in the value structure
+    newitem->pair.value.parameters = params;             // Set the parameters in the value structure
+    newitem->next = NULL;                               // Initialize the next pointer to NULL
+    t->size++;                                          // Increment the number of elements in the symbol table
 
-    size_t index = symtableHash(key) % t->arr_size;
+    size_t index = symtableHash(key) % t->arr_size; // Calculate the index in the array of linked lists
     SymtableItem *tmp = t->arr_ptr[index];
 
     if (tmp == NULL)
-        t->arr_ptr[index] = newitem;
+        t->arr_ptr[index] = newitem;                // If the linked list is empty, set the new item as the head
     else
     {
         while (tmp->next != NULL)
-            tmp = tmp->next;
-
-        tmp->next = newitem;
+            tmp = tmp->next;                        // Traverse to the end of the linked list
+        tmp->next = newitem;                        // Set the new item as the last element in the linked list
     }
 
-    return &(newitem->pair);
+    return &(newitem->pair);                        // Return the added pair
 }
 
+// Function to free the memory used by the symbol table
 void symtableFree(Symtable *t)
 {
     if (t == NULL)
@@ -120,14 +124,14 @@ void symtableFree(Symtable *t)
         while (curr != NULL)
         {
             next = curr->next;
-            free((char *)curr->pair.key);
-            free(curr);
-            curr = next;
+            free((char *)curr->pair.key);          // Free the memory used by the key
+            free(curr);                            // Free the memory used by the current item
+            curr = next;                           // Move to the next item in the linked list
         }
-        t->arr_ptr[i] = NULL;
+        t->arr_ptr[i] = NULL;                      // Set the linked list to NULL after freeing all items
     }
-    t->size = 0;
+    t->size = 0;                                   // Reset the number of elements to 0
 
-    free(t->arr_ptr);
-    free(t);
+    free(t->arr_ptr);                              // Free the memory used by the array of linked lists
+    free(t);                                       // Free the memory used by the symbol table
 }
